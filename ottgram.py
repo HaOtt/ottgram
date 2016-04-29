a0c8d60bb16a5b08a5fae1f04873ea36b55fe7c3
otth <otth@edu.htl-villach.at>
2016-04-29 21:41:47 +0200

    Image manipulation

--- ottgram.py
+++ ottgram.py
@@ -1,27 +1,32 @@
 import sys
 import time
 import telepot
-from pprint import pprint
+from PIL import Image
+
 
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     if content_type == 'text':
-        pprint(msg)
         text = msg['text']
         if text == '...':
             bot.sendMessage(chat_id, 'FUCK YOU!!')
         else:
             bot.sendMessage(chat_id, '...')
     elif content_type == 'photo':
-        pprint(msg)
-
-
+        photo = msg['photo'][3]['file_id']
+        fp = 'ottgram_image.jpg'
+        bot.download_file(photo, fp)
+        im = Image.open(fp)
+        im.rotate(180).save(fp)
+        f = open(fp, 'rb')
+        bot.sendPhoto(chat_id, f)
+        f.close()
 
 TOKEN = sys.argv[1]
 
 bot = telepot.Bot(TOKEN)
 bot.message_loop(handle)
-print ('Listening ...')
+print('Listening ...')
 
 # Keep the program running.
 while 1:
